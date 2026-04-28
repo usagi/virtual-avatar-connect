e097649d3a3ca32ffee6927044898ae25e2bf8a3
Usagi Network <the@usagi.network>
2026-04-28 19:48:41 +0900

    refactor(bin): return runner errors directly

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -1,5 +1,4 @@
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
-	virtual_avatar_connect::run_cli().await.unwrap();
-	Ok(())
+async fn main() -> virtual_avatar_connect::Result<()> {
+	virtual_avatar_connect::run_cli().await
 }
--- src/bin/desktop.rs
+++ src/bin/desktop.rs
@@ -1,6 +1,5 @@
 #![cfg_attr(all(windows, not(debug_assertions)), windows_subsystem = "windows")]
 
-fn main() -> std::io::Result<()> {
-	virtual_avatar_connect::run_desktop().unwrap();
-	Ok(())
+fn main() -> virtual_avatar_connect::Result<()> {
+	virtual_avatar_connect::run_desktop()
 }
